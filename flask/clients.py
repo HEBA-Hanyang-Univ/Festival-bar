cb94d304e4d1f1f2446b37774a73b77414329f4f
gnstjdwh <gnsgns0912@naver.com>
2023-10-27 14:12:16 +0900

    Revert "Update controller.py , clients.py"
    This reverts commit 871d60b677a88d03789f0b311f651ea3a3fb7116.

--- flask/clients.py
+++ flask/clients.py
@@ -35,7 +35,7 @@ def get_all(load_json=True, check_token=True):
     if check_token == True:
         token = data.get('token')
         if controller.get_table(controller.get_table_no_by_token(token)) == None:
-            output['result'] = "fail"
+            output['result'] = {"fail" : "Invalid token"}
             return output
 
     if load_json == True:
@@ -64,7 +64,7 @@ def get_table():
         output['result'] = table_info
         return output
     else:
-        output['result'] = "fail"
+        output['result'] = {'error': 'Token is missing or invalid'}
         return output
 
 
@@ -92,7 +92,7 @@ def set_table():
         output['result'] = result
         return output
     else:
-        output['result'] = "fail"
+        output['result'] = {"fail" : "Invalid token"}
         return output
 
 
@@ -155,7 +155,7 @@ def reject():
         output['result'] = controller.reject(table_no, reject_table)
         return output
     else:
-        output['result'] = "fail"
+        output['result'] = {"fail" : "Invalid token"}
         return output
 
 
@@ -195,6 +195,6 @@ def call():
         output['result'] = controller.call(table_no, join)
         return output
     else:
-        output['result'] = "fail"
+        output['result'] = {"fail" : "Invalid token"}
         return output
 
