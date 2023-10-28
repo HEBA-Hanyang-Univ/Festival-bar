7c1fe0dcffadc6a0fc0e386651ec0811cee394f4
bigmooon <bigmooon1215@gmail.com>
2023-10-28 18:31:32 +0900

    add Design to Checkin.js add friendCode to Checkin.js

--- react/heba-festival/src/pages/CheckIn.js
+++ react/heba-festival/src/pages/CheckIn.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";  
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";  
+import { Link } from "react-router-dom";
 import "styles/CheckIn.scss";
 import "styles/common.scss";
-import Male from "assets/images/male.png";
-import Female from "assets/images/female.png";
-import Mixed from "assets/images/mixed.png";
+import Man from "assets/images/Man.svg";
+import ManSelected from "assets/images/ManSelcted.svg";
+import Woman from "assets/images/Woman.svg";
+import WomanSelected from "assets/images/WomanSelected.svg";
+import Couple from "assets/images/Couple.svg";
+import CoupleSelected from "assets/images/CoupleSelected.svg";
 import QuantityControl from "components/QuantityControl";
-import secureLocalStorage from "react-secure-storage";
 
 //  TODO : 서버로부터 테이블 번호 요청 후 렌더링
 
@@ -14,17 +16,9 @@ export const CheckIn = () => {
   const [selectedGender, setSelectedGender] = useState(null);
   const [quantity, setQuantity] = useState(0);
   const [introduce, setIntroduce] = useState("");
+  const [friendCode, setfriendCode] = useState("");
   const [agree, setAgree] = useState(false);
-  const token = secureLocalStorage.getItem('token');
-  const navigate = useNavigate();
-  // 에러메시지 상태 추가
   
-  useEffect(() => {
-    if (token == null) {
-      navigate('/error');
-    }
-  });
-
   // 하나의 성별만 선택
   const handleGenderSelect = (gender) => {
     setSelectedGender(gender);
@@ -43,6 +37,10 @@ export const CheckIn = () => {
     setIntroduce(event.target.value);
   }
 
+  const handleFriendCodeChange = (event) => {
+    setfriendCode(event.target.value);
+  }
+
   const handleAgreeChange = (event) => {
     setAgree(event.target.checked);
   }
@@ -50,47 +48,42 @@ export const CheckIn = () => {
   const handleSubmit = (e) => {
     e. preventDefault();
 
-    if (selectedGender==null || quantity == 0) return;
-
     const data = {
-      token: token,
       gender: selectedGender,
       nums: quantity,
       note: introduce,
       photo: agree,
-      referrer: "", // TODO : set referrer
     };
 
-    fetch("http://150.230.252.177:5000/set-table", {
-      mode:'cors',
+    fetch("/set-table", {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
         "Content-Type": "application/json",
       },
     })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log("서버 응답: ", result);
 
-      if (result.result && result.result.error) {
-        console.error("서버 에러: ", result.reult.error)
-      }
-    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("서버 응답: ", result);
+
+        if (result.result && result.result.error) {
+          console.error("서버 에러: ", result.reult.error)
+        }
+      })
 
   }
 
   return (
     <div id="checkInWrap">
       <div className="checkInTitle">
-        <span>Update Your Profile</span>
+        <span>프로필을 설정하세요</span>
       </div>
       <form onSubmit={(e) => e.preventDefault()}>
         <div className="checkInButtonBox">
           <div className="selectBox genderSelect">
             <div className="selectBoxTitle">
-              <span className="highlight">*</span>
-              <span>성별</span>
+              <span>성별을 선택하세요</span>
             </div>
             <div className="genderBtn">
               <button
@@ -98,28 +91,42 @@ export const CheckIn = () => {
                 data-name="male"
                 onClick={() => handleGenderSelect("male")}
               >
-                <img src={Male} style={{width:'2.125rem',height:'2.8125rem'}} alt="male img"/>
+                {selectedGender === "male" ? (
+                  <img src={ManSelected} style={{width: '5.5rem', height: '4.5rem', marginBottom: '0.6rem'}} alt="male selected img" />
+                ) :
+                  <img src={Man} style={{width: '5.5rem', height: '4.5rem', marginBottom: '0.6rem'}} alt="male img" />
+                }
+                <span>남자</span>
               </button>
               <button
                 className={`customRadioBtn femaleBtn ${selectedGender === "female" ? "selected" : ""}`}
                 data-name="female"
                 onClick={() => handleGenderSelect("female")}
               >
-                <img src={Female} style={{width:'2.625rem',height:'2.625rem'}} alt="female img"/>
+                {selectedGender === "female" ? (
+                  <img src={WomanSelected} style={{width: '6.5rem', height: '4.5rem', marginBottom: '0.6rem'}} alt="female selected img" />
+                ) :
+                  <img src={Woman} style={{width: '6.5rem', height: '4.5rem', marginBottom: '0.6rem'}} alt="female img" />
+                }
+                <span style={{marginLeft: '2rem'}}>여자</span>
               </button>            
               <button
                 className={`customRadioBtn mixedBtn ${selectedGender === "mixed" ? "selected" : ""}`}
                 data-name="mixed"
                 onClick={() => handleGenderSelect("mixed")}
               >
-                <img src={Mixed} style={{width:'5rem',height:'2.5rem'}} alt="mixed img"/>
+                {selectedGender === "mixed" ? (
+                  <img src={CoupleSelected} style={{width: '9rem', height: '5.3rem', marginTop: '0.6rem', marginBottom: '0.23rem'}} alt="mixed selected img" />
+                ) :
+                  <img src={Couple} style={{width: '9rem', height: '5.3rem', marginTop: '0.6rem', marginBottom: '0.4rem'}} alt="mixed img" />
+                }                
+                <span style={{marginLeft: '3rem'}}>혼성</span>
               </button>
             </div>
           </div>
           <div className="selectBox headSelect">
             <div className="selectBoxTitle">
-              <span className="highlight">*</span>
-              <span>인원 수</span>
+              <span>인원 수를 선택하세요</span>
             </div>
             <QuantityControl
               quantity={quantity}
@@ -130,36 +137,46 @@ export const CheckIn = () => {
         </div>
         <div className="selfIntroduceContent">
           <div className="selfIntroduceTitle">
-            <span>한 줄 소개(선택 사항)</span>
+            <span className="spanMiddle">한줄 소개</span>
+            <span className="spanSmall">(선택 사항)</span>
           </div>
-          <textarea name="introduce" cols={"45"} rows={"3"} spellCheck="false" value={introduce} onChange={handleIntroduceChange}></textarea>
+          <textarea name="introduce" cols={"42"} rows={"1"} spellCheck="false" value={introduce} onChange={handleIntroduceChange}></textarea>
+        </div>
+        <div className="friendCode">
+          <div className="friendCodeTitle">
+            <span className="spanMiddle">지인 코드 입력</span>
+            <span className="spanSmall">(선택 사항)</span>
+          </div>
+          <textarea name="friendCode" cols={"42"} rows={"1"} spellCheck="false" value={friendCode} onChange={handleFriendCodeChange}></textarea>
         </div>
         <div className="filmAgree">
           <div className="filmAgreeTitle">
-            <span>촬영 동의서 (선택 사항)</span>
+            <span className="spanMiddle">촬영 동의서</span>
+            <span className="spanSmall">(선택 사항)</span>
           </div>
           <div className="filmAgreeContent">
-            <h4>저희 주점에 오신 것을 환영합니다!</h4><br></br>
-            안녕하세요 먼저 저희 바른생각 바른주점에 찾아와 주신 모든 분들께 감사드립니다.
-            <br></br><br></br>
-            다름이 아니라, 저희 주점에서 미디어 홍보 자료 제작을 위해 촬영을 진행하고자 합니다.
+            저희 주점에 오신 것을 환영합니다!
             <br></br>
-            촬영에 동의하시는 테이블에는 ~~의 혜택을 드리고 있으니, 괜찮으시다면 동의 체크박스를 눌러주세요!
-            <br></br><br></br>
-            저희 주점에서 즐거운 추억 많이 만들어가시길 바랍니다.
+            안녕하세요. 바른주점에 찾아와주셔서 감사합니다.
+            <br></br>
+            저희 주점에서는 미디어 홍보 자료 제작을 위해 촬영을 진행하고 있습니다.
+            <br></br>
+            촬영에 동의하시는 테이블에는 소정의 혜택을 드리고 있으니, 괜찮으시다면 동의를 체크해주세요.
+            <br></br>
+            저희 주점에서 좋은 인연과 추억 많이 만들어 가시면 좋겠습니다.
             <br></br>
             감사합니다.
           </div>
           <div className="agreeCheckBox">
             <input type="checkbox" id="agree" name="agree" checked={agree} onChange={handleAgreeChange}></input>
-            <label htmlFor="agree">Agree</label>
+            <label htmlFor="agree">동의합니다</label>
           </div>
         </div>
-        <button className="submitBtn" type="submit" value={"Submit"} onClick={handleSubmit}>
+        <button className="submitBtn" type="submit" value={"Submit"}>
           {selectedGender && quantity > 0 ? (
             <Link to={"/landing"} style={{textDecorationLine: "none",}}><span>Submit</span></Link>
           ) : (
-            <span>Submit</span>
+            <span>제출하기</span>
           )}
         </button>
       </form>
