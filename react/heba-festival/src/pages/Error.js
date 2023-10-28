3b96bfc476fe83c4df273a995785d43b33414da1
bigmooon <bigmooon1215@gmail.com>
2023-10-28 18:52:00 +0900

    add design to error.js

--- react/heba-festival/src/pages/Error.js
+++ react/heba-festival/src/pages/Error.js
@@ -1,26 +1,28 @@
 import React from "react";
 import "styles/Error.scss";
 import "styles/common.scss";
-import ErrorImg from "assets/images/error.png";
+import ErrorImg from "assets/images/Tiger.svg";
 import secureLocalStorage from "react-secure-storage";
 
 
 export const Error = () => {
   secureLocalStorage.clear(); // remove all the local storage if error occurs
   return (
-    <div className="errorWrap">
-      <div className="errorBox">
-        <div className="errorImg">
-          <img src={ErrorImg} alt="error img"></img>
-        </div>
-        <div className="errorMessage">
-          <span>
-            일시적인 오류가 발생하였습니다.
-            <br></br>
-            QR 코드를 다시 찍어주세요!
-            <br></br>
-            이용에 불편을 드려 죄송합니다.
-          </span>
+    <div className="errorOverlay">
+      <div className="errorWrap">
+        <div className="errorBox">
+          <div className="errorImg">
+            <img src={ErrorImg} alt="error img"></img>
+          </div>
+          <div className="errorMessage">
+            <span>
+              크앙! 일시적인 오류다흥!
+              <br></br>
+              QR 코드를 다시 찍으라흥!
+              <br></br>
+              이용에 불편을 줘서 미안하다흥!
+            </span>
+          </div>
         </div>
       </div>
     </div>
