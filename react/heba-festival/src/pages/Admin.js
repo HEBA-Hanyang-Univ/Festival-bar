0c2f32d9cf1710b3e78e0dda92491a58fa3cec81
sujinee01 <kmosujin@gmail.com>
2023-11-06 00:45:52 +0900

    Admin 페이지 디자인 및 토글 수정

--- react/heba-festival/src/pages/Admin.js
+++ react/heba-festival/src/pages/Admin.js
@@ -15,7 +15,6 @@ import ExitTableModal from "components/Modal/AdminModal/ExitTableModal";
 import JoinTableModal from "components/Modal/AdminModal/JoinTableModal";
 
 function Admin() {
-
   const [isOpenTableInfoModal, setIsOpenTableInfoModal] = useState(false);
   const [isOpenTimeModal, setIsOpenTimeModal] = useState(false);
   const [isOpenHeartModal, setIsOpenHeartModal] = useState(false);
@@ -34,71 +33,87 @@ function Admin() {
   // a function for processing data after fetching
   // count each table's number and calculate remained time
   function postProcessData(datas) {
-    tableNums = { male:0, female:0, mixed:0, joined:0, empty:0,};
+    tableNums = { male: 0, female: 0, mixed: 0, joined: 0, empty: 0 };
     const currentTime = new Date();
 
     const processIndividualData = (data) => {
-      const remainedTime = (new Date(data.end_time).getTime() - currentTime) / 1000;
+      const remainedTime =
+        (new Date(data.end_time).getTime() - currentTime) / 1000;
 
       // count for each tables
       if (data.join) {
-	tableNums.joined += 1;
-      } else if (data.gender === "male" || data.gender === "female" || data.gender === "mixed") {
-	tableNums[data.gender] += 1;
+        tableNums.joined += 1;
+      } else if (
+        data.gender === "male" ||
+        data.gender === "female" ||
+        data.gender === "mixed"
+      ) {
+        tableNums[data.gender] += 1;
       } else {
-	tableNums.empty += 1;
+        tableNums.empty += 1;
       }
       // translate data to AdminTable
       // TODO : add managerCall by alarmData
       return (
-	<AdminTable tableNumber={ data.table_no } gender={ data.gender } headCount={ data.nums } 
-	huntingSuccess={ data.join } remainedTime={ remainedTime } managerCall={ false } onClickTable={ onClickTableElem } />
+        <AdminTable
+          tableNumber={data.table_no}
+          gender={data.gender}
+          headCount={data.nums}
+          huntingSuccess={data.join}
+          remainedTime={remainedTime}
+          managerCall={false}
+          onClickTable={onClickTableElem}
+        />
       );
-    }
+    };
     return datas.map((data) => processIndividualData(data));
   }
 
-  // variables that related to data fetching
-  // !!! DO NOT MODIFY !!!
+  // // variables that related to data fetching
+  // // !!! DO NOT MODIFY !!!
   const { token } = useParams();
   const navigate = useNavigate();
 
   // Refetch query
   const queryclient = useQueryClient();
-  const{ data, isLoading, error, isFetching } = useQuery({
-    queryKey : ['get-all'] ,
+  const { data, isLoading, error, isFetching } = useQuery({
+    queryKey: ["get-all"],
     queryFn: async () => {
-      const response = await fetch('http://150.230.252.177:5000/get-all', {
-        mode : 'cors',
-        method : 'POST',
-        headers : {'Content-Type': 'application/json',},
-        body : JSON.stringify({
-          'token': token,
+      const response = await fetch("http://150.230.252.177:5000/get-all", {
+        mode: "cors",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          token: token,
         }),
-      })
-      .then((res) => res.json())
+      }).then((res) => res.json());
       return response;
     },
-    select : (data) => {
-      if (data.result && data.result !== 'fail') {
+    select: (data) => {
+      if (data.result && data.result !== "fail") {
         return postProcessData(data.result);
       } else {
-	navigate('/error');
-	return [];
+        navigate("/error");
+        return [];
       }
     },
-    refetchInterval : 1000, // data refetch for every 1 sec.
-    refetchIntervalInBackground : true,
-    initialData : () => {
+    refetchInterval: 1000, // data refetch for every 1 sec.
+    refetchIntervalInBackground: true,
+    initialData: () => {
       // this seems to be not working as intended...
-      return (
-        Array.from({ length: 30 }, (_, i) => {
-	  return({ table_no: i+1, gender: "", active: false, nums: 0, join: false, referrer: "", });
-        })
-      );
+      return Array.from({ length: 30 }, (_, i) => {
+        return {
+          table_no: i + 1,
+          gender: "",
+          active: false,
+          nums: 0,
+          join: false,
+          referrer: "",
+        };
+      });
     },
   });
-  
+
   // 박스 클릭시 나오는 모달창
   const onClickButton = (modalType) => {
     if (modalType === "tableInfo") {
@@ -131,7 +146,7 @@ function Admin() {
   // handle both multiple and single selection of tables
   // if multiple mode is inactive, TableInfoModal will be opened
   // if multiple mode is active, selection mode will be executed
-  const [isMultipleSelectMode, setIsMultipleSelectMode] = useState(false); 
+  const [isMultipleSelectMode, setIsMultipleSelectMode] = useState(false);
   const [selectedTable, setSelectedTable] = useState([]);
 
   function onClickTableElem(event) {
@@ -139,27 +154,37 @@ function Admin() {
 
     if (isMultipleSelectMode) {
       if (selectedTable.includes(event.currentTarget)) {
-	setSelectedTable(selectedTable.filter(table => table !== event.currentTarget));
+        setSelectedTable(
+          selectedTable.filter((table) => table !== event.currentTarget)
+        );
       } else {
-	setSelectedTable([...selectedTable, event.currentTarget]);
+        setSelectedTable([...selectedTable, event.currentTarget]);
       }
     } else {
       setIsOpenTableInfoModal(true);
     }
-  };
-  
+  }
+  const [buttonStyle, setButtonStyle] = useState({
+    backgroundColor: "#87deff",
+    color: "#fff",
+  });
   const onClickTableSelectButton = () => {
-    // setState is asnychronous, so check value before set reverse
+    let buttonStyle = {}; // 함수 내부에서 변수 선언
+
     if (isMultipleSelectMode) {
       setSelectedTable([]);
+      setButtonStyle({
+        backgroundColor: "#fff",
+        color: "#87deff",
+      });
     } else {
-      // when multiple select mode is enabled
-      // you can add code here
+      setButtonStyle({
+        backgroundColor: "#87deff",
+        color: "#fff",
+      });
     }
     setIsMultipleSelectMode(!isMultipleSelectMode);
-  }
-  
-
+  };
   /*  TODO: 알람데이터 연결 + 알람 타입 넣어주기 */
   let [alarmData, setAlarmData] = useState([
     {
@@ -185,7 +210,7 @@ function Admin() {
   ]);
 
   // TODO: this function's name is ambiguous... should change it to onDeleteAlarm
-  const onDelete = (index) => {
+  const onDeleteAlarm = (index) => {
     // 삭제 로직을 구현합니다.
     // 예를 들어, alarmData 배열에서 index에 해당하는 항목을 제거할 수 있습니다.
     const updatedAlarmData = [...alarmData];
@@ -206,19 +231,21 @@ function Admin() {
       return () => clearInterval(intervalId);
     }, []);
 
-    const formattedDateTime = currentDateTime.toLocaleString();
+    const formattedDate = currentDateTime.toLocaleDateString();
+    const hours = currentDateTime.getHours();
+    const minutes = currentDateTime.getMinutes();
+    const formattedHours = hours < 10 ? `0${hours}` : hours;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const period = hours >= 12 ? "PM" : "AM";
+
+    const formattedTime = `${formattedHours}:${formattedMinutes} ${period}`;
 
     return (
       <div>
-        <p>{formattedDateTime}</p>
+        <span className="header-date">{formattedDate}&nbsp;&nbsp;</span>
+        <span className="header-time">{formattedTime}</span>
       </div>
     );
-  };
-
-  const [selectedOption, setSelectedOption] = useState("1");
-
-  const handleChange = (event) => {
-    setSelectedOption(event.target.value);
   };
 
   return (
@@ -250,7 +277,10 @@ function Admin() {
           {alarmData.map((item, index) => {
             return (
               <div key={index} className="alarm-item">
-                <button className="alarmdel" onClick={() => onDelete(index)}>
+                <button
+                  className="alarmdel"
+                  onClick={() => onDeleteAlarm(index)}
+                >
                   <span className="alarmbtn">x</span>
                 </button>
                 <br />
@@ -284,11 +314,11 @@ function Admin() {
         <div class="bottom-line"></div>
       </header>
       <div class="table-list">
-        <div class="table-man">{ tableNums.male }</div>
-        <div class="table-woman">{ tableNums.female }</div>
-        <div class="table-mixed">{ tableNums.mixed }</div>
-        <div class="table-join">{ tableNums.joined }</div>
-        <div class="table-empty">{ tableNums.empty }</div>
+        <div class="table-man">{tableNums.male}</div>
+        <div class="table-woman">{tableNums.female}</div>
+        <div class="table-mixed">{tableNums.mixed}</div>
+        <div class="table-join">{tableNums.joined}</div>
+        <div class="table-empty">{tableNums.empty}</div>
       </div>
 
       <div class="admin_nav">
@@ -314,12 +344,12 @@ function Admin() {
       </div>
 
       {/* TODO: add AdminTable container style */}
-      <div class="table-container">
-	{ React.Children.toArray(data) }
-      </div>
+      <div class="table-container">{React.Children.toArray(data)}</div>
       {isOpenTableInfoModal && (
-        <TableInfoModal open={isOpenTableInfoModal}
-          onClose={() => onCloseModal("tableInfo")}></TableInfoModal>
+        <TableInfoModal
+          open={isOpenTableInfoModal}
+          onClose={() => onCloseModal("tableInfo")}
+        ></TableInfoModal>
       )}
 
       {/* TODO: 테이블의 시간추가, 하트추가 등 기능 연결 */}
@@ -329,28 +359,49 @@ function Admin() {
             시간 추가
           </button>
           {isOpenTimeModal && (
-            <TimeModal open={isOpenTimeModal} onClose={() => onCloseModal("time")}></TimeModal>
+            <TimeModal
+              open={isOpenTimeModal}
+              onClose={() => onCloseModal("time")}
+            ></TimeModal>
           )}
           <button className="heart-plus" onClick={() => onClickButton("heart")}>
             하트 충전
           </button>
           {isOpenHeartModal && (
-            <HeartModal open={isOpenHeartModal} onClose={() => onCloseModal("heart")}></HeartModal>
+            <HeartModal
+              open={isOpenHeartModal}
+              onClose={() => onCloseModal("heart")}
+            ></HeartModal>
           )}
           <button className="table-exit" onClick={() => onClickButton("exit")}>
             퇴장 처리
           </button>
           {isOpenExitModal && (
-            <ExitTableModal open={isOpenExitModal} onClose={() => onCloseModal("exit")}></ExitTableModal>
+            <ExitTableModal
+              open={isOpenExitModal}
+              onClose={() => onCloseModal("exit")}
+            ></ExitTableModal>
           )}
-          <button className="table-mix" onClick={() => onClickButton("joinTable")}>
+          <button
+            className="table-mix"
+            onClick={() => onClickButton("joinTable")}
+          >
             합석 처리
           </button>
           {isOpenJoinTableModal && (
-            <JoinTableModal open={isOpenJoinTableModal} onClose={() => onCloseModal("joinTable")}></JoinTableModal>
+            <JoinTableModal
+              open={isOpenJoinTableModal}
+              onClose={() => onCloseModal("joinTable")}
+            ></JoinTableModal>
           )}
-          {/* TODO: 테이블 선택을 눌렀을 때 적용할 코드 적용 */} 
-          <button class="table_choice" onClick={ onClickTableSelectButton }>테이블 선택</button>
+          {/* TODO: 테이블 선택을 눌렀을 때 적용할 코드 적용 */}
+          <button
+            className="table_choice"
+            style={buttonStyle}
+            onClick={onClickTableSelectButton}
+          >
+            테이블 선택
+          </button>
         </div>
       </div>
     </div>
